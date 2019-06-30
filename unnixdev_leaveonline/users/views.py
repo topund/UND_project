a6e81030ea0fc7c670b540ee4f57e81907072934
puntakarn <a5810091a@gmail.com>
2019-06-30 15:47:29 +0700

    ทำ login ผ่าน line เสร็จ

--- unnixdev_leaveonline/users/views.py
+++ unnixdev_leaveonline/users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 
@@ -45,16 +46,39 @@ def registerLine(request):
         body = get_profile.json()
         # User.objects.filter(pk=request.user.id).update(first_name=body['given_name'], last_name=body['family_name'])
 
-        dep = Department.objects.filter(dep_name=body["department"])
-        pos = Position.objects.filter(pos_name=body["position"])
-        sex = Sextype.objects.filter(sex_type=body["sex"])
 
-        proobj =  Profile(
-                    user_id=request.user.id,
-                    dep_name_id=dep[0].id,
-                    pos_name_id=pos[0].id,
+        dep = Department.objects.filter(dep_name=body["department"])
+        body["department"] = dep[0].id
+        pos = Position.objects.filter(pos_name=body["position"])
+        body["position"] = pos[0].id
+        body["position_name"] = str(pos[0]) 
+        sex = Sextype.objects.filter(sex_type=body["sex"])
+        body["sex"] = sex[0].id
+        body["line"] = encrypt_decrypt(request.GET['token'], "decrypt")
+        
+        profile_obj = Profile.objects.filter(user_id=request.user.id)
+        if not profile_obj:
+            print("-----------------------------------------")
+            print("------------ don't register -------------")
+            print("-----------------------------------------")
+            insertProfile(body, request.user.id)
+            insertToRemainpol(body, request.user.id)
+        else:
+            # update id line
+            print("-----------------------------------------")
+            print("------------ update line ----------------")
+            print("-----------------------------------------")
+            profile_obj.update(line=body["line"])
+        
+        return redirect(reverse('frontend:index'))
+
+def insertProfile(body, user_id):
+    proobj =  Profile(
+                    user_id=user_id,
+                    dep_name_id=body["department"],
+                    pos_name_id=body["position"],
                     status_work_id=1,
-                    sex_id=sex[0].id,
+                    sex_id=body["sex"],
                     firstname = body['given_name'],
                     lastname = body['family_name'],
                     nickname = body["nickname"],
@@ -62,18 +86,16 @@ def registerLine(request):
                     dateofstart = datetime.datetime.now(),
                     phone = body["phone"],
                     address = "Address null",
-                    line = encrypt_decrypt(request.GET['token'], "decrypt")
+                    line = body["line"]
                 )
         
-        proobj.save()
-        data_dump = {
-            "sex" : body["sex"],
-            "department":dep.id,
-            "position":pos.id
-        }
-        insertToRemainpol(data_dump, request.user.id)
-        
-        return JsonResponse({"REgister":"sss"})
+    proobj.save()
+    if("management" == body["position_name"]):
+        supervisor = Suppervisor(
+            dep_name_id = body["department"],
+            sup_name_id = user_id,
+        )
+        supervisor.save()
 
 def insertToRemainpol(jsonreq, user_id):
     type_policy = list(Policytype.objects.all().values('id','policy_name','policy_key'))
@@ -81,15 +103,16 @@ def insertToRemainpol(jsonreq, user_id):
     pos_id = Position.objects.get(pos_name = "all")
 
     sex_type = Sextype.objects.get(id = jsonreq["sex"])
-
+    print(sex_type)
     print(dep_id)
     print(pos_id)
     print(user_id)
+
     for i in range(0,len(type_policy)):
-        
+
         print(type_policy[i]['policy_name'])
         print(type_policy[i]['policy_key'])
-        if(type_policy[i]['policy_key'] == "maternitym" and str(sex_type) == "male"):
+        if(type_policy[i]['policy_key'] == "maternity" and str(sex_type) == "male"):
             print("male")
             continue
 
